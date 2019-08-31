050dba55a569d57fb43e21182299efe19b09f56f
Henning Jacobs <henning@zalando.de>
2019-08-31 21:47:21 +0200

    v0.6.0 now works for helloworld

--- examples/aiohttp-helloworld/tests/test_web.py
+++ examples/aiohttp-helloworld/tests/test_web.py
@@ -4,7 +4,11 @@ import requests
 def test_web_hello_world(kind_cluster):
     kind_cluster.load_docker_image("hjacobs/aiohttp-helloworld:latest")
     kind_cluster.kubectl("apply", "-f", "deployment.yaml")
+
+    # wait for rolling update
     kind_cluster.kubectl("rollout", "status", "deployment/aiohttp-helloworld")
+
+    # now check whether our app returns the expected text
     with kind_cluster.port_forward("service/aiohttp-helloworld", 80) as port:
         response = requests.get(f"http://localhost:{port}/")
         response.raise_for_status()
