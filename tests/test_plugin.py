fd563712923d7107fbece52798524cd9777629de
Henning Jacobs <henning@zalando.de>
2019-12-10 10:50:37 +0100

    update expected K8s version

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -7,10 +7,10 @@ def test_kind_cluster(testdir):
     import socket
 
     def test_cluster_api(kind_cluster):
-        assert kind_cluster.api.version == ('1', '15')
+        assert kind_cluster.api.version == ('1', '16')
 
     def test_kubectl_version(kind_cluster):
-        assert "v1.15" in kind_cluster.kubectl("version")
+        assert "v1.16" in kind_cluster.kubectl("version")
 
     def test_load_docker_image(kind_cluster):
         kind_cluster.load_docker_image("busybox")
